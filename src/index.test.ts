0a9cdd2da45aa955ca51669595870d25d45f1cb8
Matthew Vernacchia <mvernacc@mit.edu>
2021-08-20 17:34:56 -0400

    Fix by adding `"esModuleInterop": true` to tsconfig.json. Based on comments from https://stackoverflow.com/q/56348626

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,8 +1,9 @@
-// This import results in Plotly being undefined in Node/Jest.
-// import Plotly from "plotly.js-dist-min";
+// This import used to result in Plotly being undefined in Node/Jest.
+// Now with `"esModuleInterop": true` in `tsconfig.json` it works in Jest.
+import Plotly from "plotly.js-dist-min";
 
 // This import works in Node/Jest.
-import * as Plotly from "plotly.js-dist-min";
+// import * as Plotly from "plotly.js-dist-min";
 
 test("import plotly", () => {
     console.log(Plotly);
